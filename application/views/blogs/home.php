b874118dbe2d62ec142ac0fcd3874860cc2434f3
jxie007 <jxie007@e.ntu.edu.sg>
2016-01-23 10:23:32 +0800

    widen the width of the body

--- application/views/blogs/home.php
+++ application/views/blogs/home.php
@@ -1,56 +1,41 @@
 <div class="main_middle">
 	<div class="left_panel">
-		<div class="technology">
-            <p>Technology</p>
-            <ul>
-                <li><a href="">1</a></li>
-                <li><a href="">2</a></li>
-                <li><a href="">3</a></li>
-            </ul>      
+		<div class="technology per_category">
+            <div class="text_and_button">
+               <p class="category">Technology</p>
+               <button type="button" class="btn btn-success button_in_left_panel">view all</button> 
+            </div>
+            <p class="info">Technology summaries compter technology I learned</p>  
+            <a href="latest_article">What is 127.0.0.1?</a>    
         </div>
 
-        <div class="learn">
+        <div class="learn per_category">
             <p>Learn to learn</p>
             <ul>
                 <li><a href="">1</a></li>
-                <li><a href="">2</a></li>
-                <li><a href="">3</a></li>
             </ul>
         </div>
 
-        <div class="quotation">
-            <p>curiosity</p>
-            <ul>
-                <li><a href="">1</a></li>
-                <li><a href="">2</a></li>
-                <li><a href="">3</a></li>
-            </ul>
-        </div>
-
-        <div class="stretch">
+        <div class="stretch per_category">
             <p>Live 100 years</p>
             <ul>
                 <li><a href="">1</a></li>
-                <li><a href="">2</a></li>
-                <li><a href="">3</a></li>
             </ul>
         </div>
 
-        <div class="english">
-            <p>Englsh matters</p>
-            <ul>
-                <li><a href="">1</a></li>
-                <li><a href="">2</a></li>
-                <li><a href="">3</a></li>
-            </ul>
+        <div class="technology per_category">
+            <div class="text_and_button">
+               <p class="category">English matters</p>
+               <button type="button" class="btn btn-success button_in_left_panel">view all</button> 
+            </div>
+            <p class="info">I believe co-location is the most important thing in learing English</p>  
+            <a href="latest_article">What is 127.0.0.1?</a>    
         </div>
 
-        <div class="drawing">
+        <div class="drawing per_category">
             <p>Jun's life</p>
             <ul>
                 <li><a href="">1</a></li>
-                <li><a href="">2</a></li>
-                <li><a href="">3</a></li>
             </ul>
         </div>
 	</div>
