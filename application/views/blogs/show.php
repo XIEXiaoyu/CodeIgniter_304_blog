5ac7e4bc3a1be44173f250c026bbf01d047a6a1b
jxie007 <jxie007@e.ntu.edu.sg>
2016-02-11 20:56:51 +0800

    let the link of each latest blog in the left panel work

--- application/views/blogs/show.php
+++ application/views/blogs/show.php
@@ -18,7 +18,7 @@
             <?php foreach($a_category_blogs as $c_b): ?>
                 <div class="blog_list">
                     <p>
-                        <a href="<?php echo site_url('blogs\blog'). '\\' . $c_b->id; ?>">
+                        <a href="<?php echo site_url('blogs/blog'). '/' . $c_b->id; ?>">
                         <?php echo $c_b->title; ?>
                         </a>
                     </p>
