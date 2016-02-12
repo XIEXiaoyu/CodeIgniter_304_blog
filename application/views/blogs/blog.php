8518f8c17e37e60f0e87051059a8d22bf1ca032b
jxie007 <jxie007@e.ntu.edu.sg>
2016-02-12 10:52:47 +0800

    add 'edit' button below blog

--- application/views/blogs/blog.php
+++ application/views/blogs/blog.php
@@ -1,4 +1,9 @@
 	<div class="content">
         <?php echo $blog[0]->content; ?>
 	</div>
+	<div>
+		<?php if(isset($_SESSION['email'])){?>
+			<a href="<?php echo site_url('blogs/edit') . '/' . $blog[0]->id; ?>" class="btn btn-link edit_article">Edit this article</a>
+		<?php } ?>
+	</div>
 </div>
