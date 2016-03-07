433400a7a8183e114dd371a991f04d28148aca6c
XIE Jun <xie_jun@ednovation.com>
2016-03-07 13:38:44 +0800

    add eloquent OEM into the project

--- application/config/database.php
+++ application/config/database.php
@@ -1,6 +1,22 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
+use Illuminate\Database\Capsule\Manager as Capsule;
+$capsule = new Capsule;
+$capsule->addConnection(array(
+    'driver'    => 'mysql',
+    'host'      => $db['default']['hostname'],
+    'database'  => $db['default']['database'],
+    'username'  => $db['default']['username'],
+    'password'  => $db['default']['password'],
+    'charset'   => 'utf8',
+    'collation' => 'utf8_unicode_ci',
+    'prefix'    => $db['default']['dbprefix'],
+));
+
+$capsule->setAsGlobal();
+$capsule->bootEloquent();
+
 /*
 | -------------------------------------------------------------------
 | DATABASE CONNECTIVITY SETTINGS
